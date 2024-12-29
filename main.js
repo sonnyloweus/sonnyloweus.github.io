58a62f67a0154dd84c626dc062c7cc1bcaf5dcea
Sonny Lowe <sonny@princeton.edu>
2024-12-28 19:05:19 -0800

    projects progress

--- main.js
+++ main.js
@@ -10,21 +10,21 @@ let caps = false;
 let shifted = false;
 let typing = false;
 
-window.addEventListener( "pageshow", function ( event ) {
-    var historyTraversal = event.persisted || 
-                            ( typeof window.performance != "undefined" && 
-                                window.performance.navigation.type === 2 );
-    if ( historyTraversal ) {
-        window.location.reload();
-    }
-});
+// window.addEventListener( "pageshow", function ( event ) {
+//     var historyTraversal = event.persisted || 
+//                             ( typeof window.performance != "undefined" && 
+//                                 window.performance.navigation.type === 2 );
+//     if ( historyTraversal ) {
+//         window.location.reload();
+//     }
+// });
 
-document.addEventListener("visibilitychange", function() {
-    if (document.hidden){
-    } else {
-        window.location.reload();
-    }
- });
+// document.addEventListener("visibilitychange", function() {
+//     if (document.hidden){
+//     } else {
+//         window.location.reload();
+//     }
+//  });
 
 document.addEventListener("DOMContentLoaded", function () {
 
